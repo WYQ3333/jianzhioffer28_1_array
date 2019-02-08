d30d12d7547cbde7a6d3675e2f9887e7337a7810
浅忆梦微凉 <3538922590@qq.com>
2019-02-08 20:55:41 +0800

    测试通过

--- 剑指offer28_1_顺序打印矩阵/test.cpp
+++ 剑指offer28_1_顺序打印矩阵/test.cpp
@@ -35,7 +35,14 @@ public:
 };
 
 void TestFunc(){
-
+	vector<vector<int>> array = { { 1, 2, 3, 4 }, {5,6,7,8}
+	, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+	Solution s;
+	vector<int> temp = s.printMatrix(array);
+	int i = 0;
+	for (i = 0; i < temp.size(); ++i){
+		cout << temp[i] <<" ";
+	}
 }
 
 int main(){
