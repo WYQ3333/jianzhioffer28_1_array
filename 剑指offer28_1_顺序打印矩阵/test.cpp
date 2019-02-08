4c6eb4525fabd2faa7fe58603e656ecc18a41db1
浅忆梦微凉 <3538922590@qq.com>
2019-02-08 20:59:49 +0800

    方法2

--- 剑指offer28_1_顺序打印矩阵/test.cpp
+++ 剑指offer28_1_顺序打印矩阵/test.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 	vector<int> printMatrix(vector<vector<int> > matrix) {
 		vector<int> ans;
@@ -34,6 +35,32 @@ public:
 	}
 };
 
+class Solution {
+public:
+	vector<int> printMatrix(vector<vector<int> > matrix) {
+		vector<int>res;
+		int n = matrix.size();
+		int m = matrix[0].size();
+		int layer = (min(m, n) - 1) / 2 + 1;
+		for (int i = 0; i<layer; i++){
+			for (int j = i; j<m - i; j++){
+				res.push_back(matrix[i][j]);
+			}
+			for (int k = i + 1; k<n - i; k++){
+				res.push_back(matrix[k][m - i - 1]);
+			}
+			for (int p = m - i - 2; (p>i - 1) && (n - i - 1 != i); p--){
+				res.push_back(matrix[n - i - 1][p]);
+			}
+			for (int q = n - i - 2; (q>i) && (m - i - 1 != i); q--){
+				res.push_back(matrix[q][i]);
+			}
+		}
+		return res;
+
+	}
+};
+
 void TestFunc(){
 	vector<vector<int>> array = { { 1, 2, 3, 4 }, {5,6,7,8}
 	, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
@@ -45,6 +72,7 @@ void TestFunc(){
 		}
 		cout << endl;
 	}
+	cout << endl;
 	Solution s;
 	vector<int> temp = s.printMatrix(array);
 	i = 0;
